44d02159283b6a05f27cf1d8d4069540fb01b888
Sofia Chernysh <lenovotab2020new@gmail.com>
2026-05-08 18:40:23 +0300

    add

--- task02/logic.cpp
+++ task02/logic.cpp
@@ -1,20 +1,18 @@
 #include "logic.h"
 
 bool is_perfect_number(int number) {
-	if (number % 2 == 1) {
+	if (number % 2 == 1 || number <= 0) {
 		return false;
 	}
 
-	int sum = 1;
+	int numbers[]{ 6, 28, 496, 8128 };
 
-	int n = number / 2;
-
-	for (int d = 2; d <= n; d++)
+	for (int i = 0; i < 4; i++)
 	{
-		if (number % d == 0) {
-			sum += d;
+		if (number == numbers[i]) {
+			return true;
 		}
 	}
 
-	return number == sum;
+	return false;
 }
